b2fa32230c702dcf0128e81042ebe443d39c89e3
Luis Vega <70670285+vegavLuis@users.noreply.github.com>
2024-08-06 09:28:41 -0600

    se corrigen errores

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,44 @@
 import { Router } from "express";
 import PDFDocument from "pdfkit";
-// import { buildPDF } from "../libs/pdfkit.js";
+import path from "path";
+import fs from 'fs-extra'
+const __dirname = path.resolve();
+
 
 const router = Router();
 
-router.get("/pdf", (req, res) => {
+router.get('/pdf', async(req, res)=> {
+  const datos = req.body
   const stream = res.writeHead(200, {
     "Content-Type": "application/pdf",
-    "Content-Disposition": "attachment; filename=prueba.pdf",
+    // "Content-Disposition": "filename=prueba.pdf",
   });
-
-  function buildPDF(dataCallback, endCallback) {
-    const doc = new PDFDocument({ bufferPage: true });
+  
+  async function buildPDF(dataCallback, endCallback) {
+    const doc = new PDFDocument({ bufferPage: true, autoFirstPage: false }
+    );
 
     doc.on("data", dataCallback);
     doc.on("end", endCallback);
-
-    doc.text(req.body.url);
-
+    
+    datos.data.forEach(element => {
+      doc.addPage()
+      doc.image(path.join(__dirname, 'images', 'flyer.png'), {
+        name: 'flyer.png',
+        type: 'image/png',
+      });
+      doc.text(element.nombre);
+    });
+      doc.pipe(fs.createWriteStream('prueba.pdf'))
+    
     doc.end();
   }
 
-  buildPDF(
+  await buildPDF(
     (data) => stream.write(data),
     () => stream.end()
   );
-  res.send("creado");
+  // res.send("creado");
 });
 
 export default router;
